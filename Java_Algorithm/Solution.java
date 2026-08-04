40fe0ed9873b2f5d8feb2700232bca23f4ca0e53
박장한 <parkjanghan2256@naver.com>
2026-08-04 17:16:24 +0900

    Java: add

--- Java_Algorithm/Solution.java
+++ Java_Algorithm/Solution.java
@@ -1,20 +1,38 @@
-import java.util.*;
-
-class Solution{
-    boolean solution(String s){
-        Stack<Character> stk = new Stack<>();
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = {};
+        int count_zero = 0;
+        int round_count = 0;
+        String ans_str = s;
         
-        for (int i = 0 ; i < s.length(); i++){
-            char c = s.charAt(i);
+        while (true){
             
-            if (!stk.isEmpty() && stk.peek() == '(' && c == ')') {
-                stk.pop();
-                continue;
-            } else {
-                stk.push(c);
-            }
-        }
+            char[] char_array = ans_str.toCharArray();
+            ans_str = "";
+            for (char ch : char_array) {
+                if (ch == '1') ans_str += "1";
+                else count_zero++;
+            }        
+            // System.out.println(ans_str);
         
-        return stk.isEmpty();
+            int str_leng = ans_str.length();
+        
+            ans_str = "";
+            while (str_leng > 0){
+                if (str_leng % 2 == 1) {
+                    ans_str = "1" + ans_str;
+                } else {
+                    ans_str = "0" + ans_str;
+                }
+                str_leng /= 2;
+            }
+            System.out.println(ans_str);
+            round_count++;
+            if (ans_str.equals("1")) break;
+        }
+        answer = new int[2];
+        answer[0] = round_count;
+        answer[1] = count_zero;
+        return answer;
     }
 }
