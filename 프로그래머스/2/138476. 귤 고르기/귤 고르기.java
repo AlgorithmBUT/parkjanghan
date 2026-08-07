08b8590e24966c71535668665a13dd09b78287fe
JANGHAN PARK <81512805+parkjanghan@users.noreply.github.com>
2026-08-07 16:23:32 +0900

    [level 2] Title: 귤 고르기, Time: 35.51 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -3,41 +3,28 @@ import java.util.*;
 class Solution {
     public int solution(int k, int[] tangerine) {
         Map<Integer, Integer> mp = new HashMap<>();
-
-		for (int i = 0; i < tangerine.length; i++) {
-			int tang = tangerine[i];
-			if (mp.containsKey(tang)) {
-				mp.put(tang, mp.get(tang) + 1);
-			} else {
-				mp.put(tang, 1);
-			}
-		}
-		
-		List<int[]> list = new ArrayList<>();
-		
-		for (Map.Entry<Integer, Integer> e : mp.entrySet()) {
-			list.add(new int[] {e.getKey(), e.getValue()});
-		}
-		
-		list.sort((a, b) -> {
-			return a[1] - b[1];
-		});
-
-		int answer = 0;
         
-		for (int i = list.size() - 1; i >= 0; i--) {
-			// System.out.println(list.get(i)[0] + " " + list.get(i)[1]);
-			if (k > list.get(i)[1]) {
-				answer++;
-				k -= list.get(i)[1];
-			} else {
-				answer++;
-				break;
-			}
-		}
-		
-		System.out.println(answer);
+        for (int i = 0; i < tangerine.length; i++){
+            int t = tangerine[i];
+            mp.put(t, mp.getOrDefault(t, 0) + 1);
+        }
         
+        // for (Entry<Integer, Integer> entry : mp){
+        //     System.out.println(entry.getKey() + " " + entry.getValue());
+        // }
+        
+        List<Integer> list = new ArrayList<>(mp.keySet());
+        
+        list.sort((a, b) -> {
+            return mp.get(b) - mp.get(a);
+        });
+        
+        int answer = 0;
+        for (Integer i : list) {
+            k -= mp.get(i);
+            answer++;
+            if (k <= 0) break;
+        }
         return answer;
     }
 }
