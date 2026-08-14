6a6df13f309ca5de4e37995a3821c72cd7aae063
JANGHAN PARK <81512805+parkjanghan@users.noreply.github.com>
2026-08-14 14:23:24 +0900

    [level 2] Title: 리코쳇 로봇, Time: 14.93 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -17,8 +17,6 @@ class Solution {
     int[] dy = {0, 1, 0, -1};
     int[] dx = {1, 0, -1, 0};
     
-    
-    
     public int solution(String[] board) {
         
         // 배열 형태로 입력
@@ -50,32 +48,32 @@ class Solution {
             int ry = p.robot_y;
             int rx = p.robot_x;
             int count = p.count;
-            visited[ry][rx] = true;
             
             // 큐 탈출 조건, Goal 지역에 로봇이 도착
             if (ry == goal_y && rx == goal_x) return count;
 
             for (int dir = 0; dir < 4; dir++){
                 // Next 값 추가 +1
-                int ny = ry + dy[dir]; 
-                int nx = rx + dx[dir];
+                int ny = ry; 
+                int nx = rx;
                 
                 while(true){
                     // 종료 조건 : 범위 나가거나 장애물 만나면 한 칸 뒤로 돌리고 종료
-                    if (ny < 0 || ny >= max_y || nx < 0 || nx >= max_x || grid[ny][nx] == 'D'){
-                        ny -= dy[dir];
-                        nx -= dx[dir];
-                        break;
+                    int nextY = ny + dy[dir];
+                    int nextX = nx + dx[dir];
+                    
+                    if (nextY < 0 || nextY >= max_y || nextX < 0 || nextX >= max_x || grid[nextY][nextX] == 'D'){                       break;
                     }
-                    ny += dy[dir];
-                    nx += dx[dir];
+                    ny = nextY;
+                    nx = nextX;
                 }
                 
                 // 이동 불가할 경우 큐에 넣지 않음
-                if (ry == ny && rx == nx) continue;
                 if (visited[ny][nx]) continue;
                 
+                visited[ry][rx] = true;
                 q.offer(new Position(ny, nx, count + 1));
+                
             }
         }
         
