b2194b700983aa5657d6827840c20e3eac13dcef
JANGHAN PARK <81512805+parkjanghan@users.noreply.github.com>
2026-08-27 09:52:42 +0900

    [level 3] Title: 네트워크, Time: 0.22 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        
+        // visited 배열 생성
+        boolean[] visited = new boolean[n];
+        
+        // stack 생성
+        Deque<Integer> stk = new ArrayDeque<>();
+        
+        // for (1 -> n 번 컴퓨터 까지)
+        for (int i = 0; i < n; i++){
+            // visited[i] == 0 이면 stack에 넣기
+            if (!visited[i]) {
+                stk.push(i);
+                visited[i] = true;
+            }
+            else continue;
+            
+            // stack()이 빌 때까지, DFS로 방문 체크
+            while(!stk.isEmpty()){
+                int cur = stk.pop();
+                
+                for (int j = 0; j < n; j++){
+                    if (!visited[j] && computers[cur][j] == 1) {
+                        visited[j] = true;
+                        stk.push(j);
+                    }
+                }
+            }
+            
+            // stack이 0이 되면 answer++
+            answer++;
+        }
+        
+        return answer;
+    }
+}
